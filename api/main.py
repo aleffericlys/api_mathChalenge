e354d854b5e5a5b03189b87e9c2ec7715a108db4
aleffericlys <ericlys558@gmail.com>
2024-09-10 17:11:08 -0300

    enviando dados por meio do body

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 
@@ -15,13 +15,12 @@ def mainroute():
 
 @App.route('/averageteste', methods=['GET'])
 def sumteste():
-	list = [1, 2, 3, 4, '6']
 	try:
-		return jsonify(average(list))
+		data = request.get_json()
+		return jsonify(average(data))
 	except Exception as e:
 		return jsonify(['Invalid value: input must be a list of integer values', str(e)])
 
 
 if __name__ == '__main__':
 	App.run(debug=True, port=3000)
-	# sumteste()
