462c32c7c27a1415f7a5b922dcac2e99bf098f3d
aleffericlys <ericlys558@gmail.com>
2024-09-10 16:19:36 -0300

    creating and testing sum and sumNumbers functions

--- api/src/library.py
+++ api/src/library.py
@@ -1,11 +1,18 @@
 def sumNumbrs(numbers: list[int]) -> int:
-	pass
+	value = 0
+	for i in numbers:
+		value = sum(value, i)
+	
+	return value
 
 def average(numbres: list[int]) -> float:
 	return sum(numbres) / len(numbres)
 
 def sum(a: int, b : int) -> int:
-	pass
+	try:
+		return a + b
+	except:
+		raise ValueError('Invalid value: all values must be integers')
 
 def divied(a: int, b : int) -> float:
 	pass
