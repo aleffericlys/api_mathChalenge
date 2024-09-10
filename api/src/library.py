6d3dd4707c111b20419b1f784aeb3918c91105aa
aleffericlys <ericlys558@gmail.com>
2024-09-10 16:18:36 -0300

    defining library functions

---
+++ api/src/library.py
@@ -0,0 +1,11 @@
+def sumNumbrs(numbers: list[int]) -> int:
+	pass
+
+def average(numbres: list[int]) -> float:
+	return sum(numbres) / len(numbres)
+
+def sum(a: int, b : int) -> int:
+	pass
+
+def divied(a: int, b : int) -> float:
+	pass
