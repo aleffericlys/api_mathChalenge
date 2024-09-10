21cd726681a45739df432a8a3060297a5d844773
aleffericlys <ericlys558@gmail.com>
2024-09-10 16:20:49 -0300

    testing the sum of values ​​in a list

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import json
 
+from src.library import sumNumbrs, average
 
 App = Flask('mathChallenge')
 App.config.from_object(__name__)
@@ -12,7 +13,12 @@ def mainroute():
 	return jsonify('Hello World')
 
 
+@App.route('/sumteste', methods=['GET'])
+def sumteste():
+	list = [1, 2, 3, 4, 540]
+	return jsonify(sumNumbrs(list))
 
 
 if __name__ == '__main__':
 	App.run(debug=True, port=3000)
+	# sumteste()
